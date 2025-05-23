687c140d45a75047bb2a1aaeab1f42bda98bfaec
Khanh1498 <132402041+Khanh1498@users.noreply.github.com>
2025-05-23 00:43:56 -0400

    Add files via upload

---
+++ build/gazebo_environment/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/jonny/github-classroom/ITU-EMAV/trajectory-tracking-control-homework-2-team-jh/install/gazebo_environment'
